a5e91613fccd1e7a2298b0d81b3f9b662f8f914a
DanielRomero <danielfelromero@outlook.com>
2020-08-11 21:42:02 -0500

    navbar list-prof

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ for(let i=0; i<logo.length; i++){
     logo[i].style.strokeDasharray = logo[i].getTotalLength();
     logo[i].style.strokeDashoffset = logo[i].getTotalLength();
     logo[i].style.animation = `line-anim var(--time-line-anim) ease forwards calc( var(--time-transition-fill)*${i})`;
+    document.querySelector('#logo').style.animation = `fill 0.5s ease forwards calc( var(--time-transition-fill)*${logo.length})`;
 }
